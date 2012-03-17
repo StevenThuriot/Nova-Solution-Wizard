31c56532a88bba3e79b6fbc5b7cb30cfd77c2f07
Steven Thuriot <steven@thuriot.be>
2012-03-17 21:59:46 +0100

    Adding the Nova Template Wizard solution.

---
+++ NovaTemplateWizard/NovaTemplateWizard/ProjectWizard.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections.Generic;
+using EnvDTE;
+using Microsoft.VisualStudio.TemplateWizard;
+
+namespace NovaTemplateWizard
+{
+	public class ProjectWizard : IWizard
+	{
+		/// <summary>
+		/// Runs custom wizard logic at the beginning of a template wizard run.
+		/// </summary>
+		/// <param name="automationObject">The automation object being used by the template wizard.</param>
+		/// <param name="replacementsDictionary">The list of standard parameters to be replaced.</param>
+		/// <param name="runKind">A <see cref="T:Microsoft.VisualStudio.TemplateWizard.WizardRunKind"/> indicating the type of wizard run.</param>
+		/// <param name="customParams">The custom parameters with which to perform parameter replacement in the project.</param>
+		public void RunStarted(object automationObject, Dictionary<string, string> replacementsDictionary, WizardRunKind runKind, object[] customParams)
+		{
+			foreach (var pair in SolutionWizard.GlobalDictionary)
+			{
+				replacementsDictionary.Add(pair.Key, pair.Value);
+			}
+		}
+
+		/// <summary>
+		/// Runs custom wizard logic when a project has finished generating.
+		/// </summary>
+		/// <param name="project">The project that finished generating.</param>
+		public void ProjectFinishedGenerating(Project project)
+		{
+			
+		}
+
+		/// <summary>
+		/// Runs custom wizard logic when a project item has finished generating.
+		/// </summary>
+		/// <param name="projectItem">The project item that finished generating.</param>
+		public void ProjectItemFinishedGenerating(ProjectItem projectItem)
+		{
+			
+		}
+
+		/// <summary>
+		/// Indicates whether the specified project item should be added to the project.
+		/// </summary>
+		/// <param name="filePath">The path to the project item.</param>
+		/// <returns>
+		/// true if the project item should be added to the project; otherwise, false.
+		/// </returns>
+		public bool ShouldAddProjectItem(string filePath)
+		{
+			return true;
+		}
+
+		/// <summary>
+		/// Runs custom wizard logic before opening an item in the template.
+		/// </summary>
+		/// <param name="projectItem">The project item that will be opened.</param>
+		public void BeforeOpeningFile(ProjectItem projectItem)
+		{
+			
+		}
+
+		/// <summary>
+		/// Runs custom wizard logic when the wizard has completed all tasks.
+		/// </summary>
+		public void RunFinished()
+		{
+			
+		}
+	}
+}
